5363f60705c70602888c145ead0602b31d385ced
Kunal Kumar <44107442+K-Dasher@users.noreply.github.com>
2022-09-30 17:40:33 +0530

    Initialise Repository

--- Day 1/Band Name Generator.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Welcome to Band Name Generator")
-    _city = input("What is the name of the city you were born in?")
-    _pet = input("What is the name of your pet?")
-    print(f"The name of your band could be: {(_city.lower()).capitalize()} {(_pet.lower()).capitalize()}")
-
-if __name__ == "__main__":
-    main()
